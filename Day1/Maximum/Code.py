4d588db53bca089b604105bef2dd7017bc82b46d
nishant8105 <nishantjadhav8105@gmail.com>
2026-01-12 10:12:24 +0530

    Remove Day
    Removed Day 1 to Restart

--- Day1/Maximum/Code.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter first number"))
-num2 = int(input("Enter second number"))
-
-if num1 > num2 :
-    print(f"{num1} number is maximum then {num2}")
-else :
-    print(f"{num2} number is maximum then {num1}")
--- Day1/N_numbers/Code.py
+++
@@ -1,5 +0,0 @@
-N = int (input("Enter the number upto print"))
-i = 0
-while i < N :
-    print (i + 1)
-    i += 1
